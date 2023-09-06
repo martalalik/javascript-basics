a751e54ecf9f58ecf32631a3862fe50e79553670
martalalik <lala.czaki@gmail.com>
2023-09-06 14:45:03 +0200

    Integers and Floating Points

--- src/index.js
+++ src/index.js
@@ -7,22 +7,21 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Numbers In-Depth
 // ----------------------------------------------------------------
 
-// Number Literal, Function and Constructor Syntax
+// Integers and Floating Points
 
-// Number Literal
-const number1 = 99; // literal number -> number in its true numeric form
-console.log(number1);
+// console.log(99.99); // 99.99
+// console.log(0.1 + 0.2 === 0.3); // false -> it is because how floating point number work due to their binary nature of their encoding.
+console.log(0.1 + 0.2); // 0.30000000000000004 -> this is how computer add numbers together
+// console.log(100 / 3); // 33.333333333333336
+// console.log(33.333333333333336 * 3); // 100
+// console.log(33.33333333333333333333333 * 3); // 100
 
-// Function
-// perform a type-conversion in a non-constructor context
-console.log(Number(55)); // 55
-console.log(Number('33')); // 33 -> trying to create a nwe number in a non constructor context.
-                                  // In other words,we are providing a type conversion,
-                                  // and we are using the number as a function and not using the new keyword.
-console.log(Number('44px')); // NaN -> not a number
 
-// Constructor Syntax
-// creates a wrapper Object
-// avoid using it
-console.log(new Number(66)); // Number {66}
-console.log(new Number('55px')); // Number {NaN}
+const price = 9.33;
+const quantity = 3;
+console.log(price * quantity); // 27.990000000000002
+
+// way of correctly rounding numbers
+const anotherPrice = 9.33 * 100; // first - removing the decimal point with *100
+const anotherQuantity = 3;
+console.log(anotherPrice * anotherQuantity / 100); // then - add the decimal point with /100 so now will give 27.99
