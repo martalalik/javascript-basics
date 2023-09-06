662de9f3d3f1cfb3a89d7cf93726be72cde88a62
martalalik <lala.czaki@gmail.com>
2023-09-06 14:25:08 +0200

    Number Literal, Function and Constructor Syntax

--- src/index.js
+++ src/index.js
@@ -4,152 +4,25 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----------------------------------------------------------------
-// Conditional logic
-
-// console.log(1 == 1); // true
-// console.log(1 === 1); // true
-//
-// console.log(1 == '1'); // true - converting both to a string, called: coercion
-// console.log(1 === '1'); // false
-//
-// console.log(Boolean(true)); // true
-// console.log(Boolean('')); // false
-// console.log(Boolean('true')); // true
-// console.log(Boolean('false')); // true
-// console.log(Boolean('any string')); // true
-//
-// console.log(Boolean(true)); // true
-// console.log(!!true); // true; called: double bang, the same as above
-// console.log(!true); // false
-//
-// // mainly truthy values
-// console.log(!!{}); // true
-// console.log(!![]); // true
-// console.log(!!new Date()); // true
-// console.log(!!'0'); // true
-// console.log(!!''); // false
-// console.log(!!0); // false
-// console.log(!!42); // true; any number above 0 gives true
-// console.log(!!-42); // true
-// console.log(!!3.14); // true
-// console.log(!!-3.14); // true
-//
-// // mainly falsy values
-// console.log(!!false); // false
-// console.log(!!null); // false
-// console.log(!!undefined); // false
-// console.log(!!0); // false
-// console.log(!!NaN); // false
-// console.log(!!''); // false
-// console.log(!!false); // false
-
+// Numbers In-Depth
 // ----------------------------------------------------------------
 
-// If, Else If and Else Statements
+// Number Literal, Function and Constructor Syntax
 
-// const condition = false;
-//
-// if (condition) {
-//   console.log('Yes'); // Yes if condition=true
-// } else {
-//   console.log('No'); // No if condition=false
-// }
-//
-// // shorter versions
-// // 1
-// if (condition) console.log('Yes');
-// else console.log('No');
-// // 2
-// condition ? console.log('Yes') : console.log('No'); // Ternary Operator
-//
-// const number = 99;
-// // const number = 1;
-// let result; // undefined
-//
-// if (number === 1) {
-//   result = 'One';
-// } else if (number === 99) {
-//   result = 'Ninety-Nine';
-// } else if (number === 1000) {
-//   result = 'One Thousand';
-// } else {
-//   result = 'No Match';
-// }
-// console.log(result);
+// Number Literal
+const number1 = 99; // literal number -> number in its true numeric form
+console.log(number1);
 
-// ----------------------------------------------------------------
+// Function
+// perform a type-conversion in a non-constructor context
+console.log(Number(55)); // 55
+console.log(Number('33')); // 33 -> trying to create a nwe number in a non constructor context.
+                                  // In other words,we are providing a type conversion,
+                                  // and we are using the number as a function and not using the new keyword.
+console.log(Number('44px')); // NaN -> not a number
 
-// Ternary Operator => condition ? true : false
-
-// const number = 1;
-// const result1 = number === 1 ? 'One' : 'No Match';
-// console.log(result1);
-//
-// // with js expression
-// const result2 = 'The Number is: ' + number === 1 ? 'One' : 'No Match'; // false - Condition is always false since types 'string' and 'number' have no overlap.
-// console.log(result2);
-// const result3 = 'The Number is: ' + (number === 1 ? 'One' : 'No Match'); // true - brackets creates expression on it's on, to evaluate on it's on.
-// console.log(result3);
-//
-// // nesting; can get confusing
-// const number1 = 99;
-// const result4 =
-//   number1 === 1
-//     ? 'One'
-//     : number1 === 99
-//       ? 'Ninety-Nine'
-//       : number1 === 1000
-//       ? 'One Thousand'
-//       : 'No Match';
-//
-// console.log(result4);
-
-// ----------------------------------------------------------------
-
-// Switch Statements
-
-// const number = 1;
-const number = '1';
-let result;
-
-switch (number) {
-  case '1':
-    result = 'One';
-    break;
-  case 99:
-    result = 'Ninety-Nine';
-    break;
-  case 100:
-    result = 'One thousand';
-    break;
-  default:
-    result = 'No Match';
-}
-console.log(result);
-
-// creating a block scope - if you need to define a variable for a case
-const number1 = 99;
-let result1;
-
-switch (number1) {
-  case 1: {
-    const text = 'One';
-    result1 = text;
-    break;
-  }
-  case 99: {
-    const text = 'Ninety-Nine';
-    result1 = text;
-    break;
-  }
-  case 100: {
-    const text = 'One thousand';
-    result1 = text;
-    break;
-  }
-  default:
-    result1 = 'No Match';
-}
-console.log(result1);
-
-// if take away {} - error: identifier 'text' has already been declared
+// Constructor Syntax
+// creates a wrapper Object
+// avoid using it
+console.log(new Number(66)); // Number {66}
+console.log(new Number('55px')); // Number {NaN}
