251999f4c4c9a91471a79da362eba5d3322045b7
martalalik <lala.czaki@gmail.com>
2023-09-06 20:00:03 +0200

    Parsing Strings to Numbers

--- src/index.js
+++ src/index.js
@@ -7,21 +7,26 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Numbers In-Depth
 // ----------------------------------------------------------------
 
-// Integers and Floating Points
+// Parsing Strings to Numbers
 
-// console.log(99.99); // 99.99
-// console.log(0.1 + 0.2 === 0.3); // false -> it is because how floating point number work due to their binary nature of their encoding.
-console.log(0.1 + 0.2); // 0.30000000000000004 -> this is how computer add numbers together
-// console.log(100 / 3); // 33.333333333333336
-// console.log(33.333333333333336 * 3); // 100
-// console.log(33.33333333333333333333333 * 3); // 100
+// parseInt(s: string, radix?: number): number
+console.log(parseInt('55px')); // 55
+console.log(parseInt('55px', 10)); // 55 -> golden rule is to pass `10` -> the number is passed with a base 10 and returns an integer that we're expecting.
+console.log(parseInt('55.9999px', 10)); // 55
 
+// parseFloat(s: string): number
+console.log(parseFloat('55.9999px')); // 55.9999
+console.log(parseFloat('55.9999px') * 10 ); // 559.999
 
-const price = 9.33;
-const quantity = 3;
-console.log(price * quantity); // 27.990000000000002
+// Number()
+console.log(Number('55e10')); // 550000000000 -> e: is a floating point number representation.
+// These aren't typically things that we would declare ourselves,
+// but we might get them as a value.
+// When we dealing with huge in JS, in this 'e' stands for exponent.
+// 10: it is 10 zeroes, 'e' adding 10 zeroes
+console.log(Number('55.9999px')); // NaN
+console.log(Number('55.9999')); // 55.9999 - it does the same thing as parseFloat()
 
-// way of correctly rounding numbers
-const anotherPrice = 9.33 * 100; // first - removing the decimal point with *100
-const anotherQuantity = 3;
-console.log(anotherPrice * anotherQuantity / 100); // then - add the decimal point with /100 so now will give 27.99
+// using operator to convert string to a number
+console.log(+'99.5555'); // 99.5555 - acting as Number()
+console.log(9 + +'99.5555'); // 108.5555 - this is confusing, adding 9 to right expression.
