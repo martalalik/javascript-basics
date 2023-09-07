1793646a1ba6ef316847bf4eae0eec64764112da
martalalik <lala.czaki@gmail.com>
2023-09-07 16:50:02 +0200

    Understanding Not-a-Number

--- src/index.js
+++ src/index.js
@@ -7,26 +7,20 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Numbers In-Depth
 // ----------------------------------------------------------------
 
-// Parsing Strings to Numbers
+// Understanding Not-a-Number
 
-// parseInt(s: string, radix?: number): number
-console.log(parseInt('55px')); // 55
-console.log(parseInt('55px', 10)); // 55 -> golden rule is to pass `10` -> the number is passed with a base 10 and returns an integer that we're expecting.
-console.log(parseInt('55.9999px', 10)); // 55
+const result = Number('55px');
+// isNan(number: number): boolean - specifically checks the value of NaN; avoid using alone without Number.isNaN()
+console.log(isNaN(result)); // true
+console.log(isNaN('I am a string')); // true, but the behavior here of isNaN is broke, because it doesn't get really a NaN but a string
 
-// parseFloat(s: string): number
-console.log(parseFloat('55.9999px')); // 55.9999
-console.log(parseFloat('55.9999px') * 10 ); // 559.999
+// Number.isNaN(number: unknown): boolean
+// but is can be fixed or properly used, was added in ES2015
+console.log(Number.isNaN(result)); // true - here specifically checking for NaN
+console.log(Number.isNaN('I am a string')); // false, now is correct
+console.log(Number.isNaN(Number.NaN)); // true
 
-// Number()
-console.log(Number('55e10')); // 550000000000 -> e: is a floating point number representation.
-// These aren't typically things that we would declare ourselves,
-// but we might get them as a value.
-// When we dealing with huge in JS, in this 'e' stands for exponent.
-// 10: it is 10 zeroes, 'e' adding 10 zeroes
-console.log(Number('55.9999px')); // NaN
-console.log(Number('55.9999')); // 55.9999 - it does the same thing as parseFloat()
+// Number.IsInteger(number: unknown): boolean -> checking if a number is indeed a number
+console.log(Number.isInteger(2)); // true
+console.log(Number.isInteger(2.7)); // false - is a floating point number
 
-// using operator to convert string to a number
-console.log(+'99.5555'); // 99.5555 - acting as Number()
-console.log(9 + +'99.5555'); // 108.5555 - this is confusing, adding 9 to right expression.
