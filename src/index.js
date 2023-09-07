0ea497371edd4d9a397a11d7e0c215861dfe1a62
martalalik <lala.czaki@gmail.com>
2023-09-07 17:12:41 +0200

    Numbers and Immutability

--- src/index.js
+++ src/index.js
@@ -7,20 +7,14 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Numbers In-Depth
 // ----------------------------------------------------------------
 
-// Understanding Not-a-Number
+// Numbers and Immutability
 
-const result = Number('55px');
-// isNan(number: number): boolean - specifically checks the value of NaN; avoid using alone without Number.isNaN()
-console.log(isNaN(result)); // true
-console.log(isNaN('I am a string')); // true, but the behavior here of isNaN is broke, because it doesn't get really a NaN but a string
-
-// Number.isNaN(number: unknown): boolean
-// but is can be fixed or properly used, was added in ES2015
-console.log(Number.isNaN(result)); // true - here specifically checking for NaN
-console.log(Number.isNaN('I am a string')); // false, now is correct
-console.log(Number.isNaN(Number.NaN)); // true
-
-// Number.IsInteger(number: unknown): boolean -> checking if a number is indeed a number
-console.log(Number.isInteger(2)); // true
-console.log(Number.isInteger(2.7)); // false - is a floating point number
+const immutableNumber = 99;
+let referencedNumber = immutableNumber;
+referencedNumber = 44; // this doesn't touch the real value of immutableNumber. 99 is never touched in this process
+// This is by design, because JS numbers are copied via their value. They're not essentially referenced, but we're making it look like they are referenced.
+// const key word has nothing to do about it, const simply means that we cannot reassign the value -> immutableNumber = 44 - error
+console.log(immutableNumber);
+console.log(referencedNumber);
+console.log(immutableNumber + 3); // 102 - but still we do not chane value of 99 because we passing primitive value by reference
 
