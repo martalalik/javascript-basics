f1dca4245482dc39e3a73557dac018f2dc7a0913
martalalik <lala.czaki@gmail.com>
2023-09-15 14:20:48 +0200

    Correctly Type-Checking Numbers, typeof, instanceof

--- src/index.js
+++ src/index.js
@@ -7,14 +7,23 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Numbers In-Depth
 // ----------------------------------------------------------------
 
-// Numbers and Immutability
+// Correctly Type-Checking Numbers
 
-const immutableNumber = 99;
-let referencedNumber = immutableNumber;
-referencedNumber = 44; // this doesn't touch the real value of immutableNumber. 99 is never touched in this process
-// This is by design, because JS numbers are copied via their value. They're not essentially referenced, but we're making it look like they are referenced.
-// const key word has nothing to do about it, const simply means that we cannot reassign the value -> immutableNumber = 44 - error
-console.log(immutableNumber);
-console.log(referencedNumber);
-console.log(immutableNumber + 3); // 102 - but still we do not chane value of 99 because we passing primitive value by reference
-
+console.log(typeof 99); // number
+console.log(typeof 99.88); // number
+console.log(99 instanceof Number); // false: 99 is literal value, is not an instance of Number
+console.log(Number('99') instanceof Number); // false (it doesn't matter if you pass 99 or '99')
+// this is because of the way that instances work and the 'new' keyword works.
+// new Number('99') : creating new instance
+console.log(new Number('99') instanceof Number); // true, there is no value in this check
+console.log({}.toString()); // [object Object] - this is irrelevant to numbers and is just a string. Here we first create a new object
+// This is the safest way to easy and reliably type check, any kind of value in JS and get very accurate representation of what it is.
+// instead of accessing object directly -> {}.toString(),
+// accessing object safely
+console.log(Object.prototype.toString()); // [object Object]. Here we access object directly without creating a new object
+console.log(Object.prototype.toString.call(99)); // [object Number] -> nice representation from the constructor of Object then the Number,
+// cal() -> change the way of executing toString() function.
+console.log(Object.prototype.toString.call(99).slice(8, -1)); // Number
+console.log(Object.prototype.toString.call(99).slice(8, -1) === 'Number'); // true, here switch case is created,
+// converting particular type into a string, then changing the execution context and takes the constructor value from the number,
+// then slicing values that are unwanted out of the string by cutting away [object ]
