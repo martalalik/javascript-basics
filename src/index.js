d2806270f37a1af89cf2b0f67446765192ef8475
martalalik <lala.czaki@gmail.com>
2023-09-19 14:44:20 +0200

    Strings and Immutability

--- src/index.js
+++ src/index.js
@@ -7,26 +7,16 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // String In-Depth
 // ----------------------------------------------------------------
 
-// String Properties and Indexes
+// Strings and Immutability
 
-// Accessing property length
-console.log(new String(5599).length); // 4
-console.log('Pepperoni'.length); // 9 -> why enter length property on a string? Many reasons, check if someone entered a credit card number. Checking length property validate amount of numbers.
-// or Finding the last character in a string:
-// how to access first character
-console.log('Pepperoni'['length']); // 9
-console.log('Pepperoni'[length]); // 'P'
-console.log('Pepperoni'[0]); // 'P' - we can pass 0 as a number or a string. Number get converted to a string, and then will give back first property. Using numeric value is preferable.
-console.log('Pepperoni'['0']); // 'P'
-
-// how to access last character
-const pizza = 'Pepperoni';
-const pizza2 = 'P e p p e r o n i';
-console.log(pizza[0]); // 'P'
-console.log(pizza.length); // 9
-console.log(pizza[pizza.length]); // undefined -> this happens because indexes are 0 based. pizza.length gives 9, so 9 won't exist (0-8).
-console.log(pizza[pizza.length -1]); // 'i' -> -1 is a fix, will gives 8.
-console.log(pizza.charAt(0)); // 'P' -> alternative for getting first character, but [] are preferred. No need to call method on a string.
-console.log(pizza2.length); // 17 spaces also count as a length property.
+const immutableString = 'I shall not change!';
+immutableString.toUpperCase(); // performing mutation to the string.
+console.log(immutableString); // "I shall not change!" - will stay immutable, because we are not using variable - uppercaseString and assigning it, we don't ever has access to it!
+// new variable where we can perform mutation.
+const uppercaseString = immutableString.toUpperCase(); // immutableString passed by value, so it takes the string literal, it takes that raw value, and
+// copies it, and then performs a toUpperCase() modification to the string.
+// Copying by the value, this means that it doesn't get in the way, or it doesn't modify or effect the original string.
+console.log(immutableString); // "I shall not change!"
+console.log(uppercaseString); // "I SHALL NOT CHANGE!"
 
 
