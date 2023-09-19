d1b0bfe34def2e7d0698bc16b265dc8daa92e39a
martalalik <lala.czaki@gmail.com>
2023-09-19 15:05:45 +0200

    Strings and Immutability

--- src/index.js
+++ src/index.js
@@ -7,16 +7,13 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // String In-Depth
 // ----------------------------------------------------------------
 
-// Strings and Immutability
+// Correctly Type-Checking Strings
 
-const immutableString = 'I shall not change!';
-immutableString.toUpperCase(); // performing mutation to the string.
-console.log(immutableString); // "I shall not change!" - will stay immutable, because we are not using variable - uppercaseString and assigning it, we don't ever has access to it!
-// new variable where we can perform mutation.
-const uppercaseString = immutableString.toUpperCase(); // immutableString passed by value, so it takes the string literal, it takes that raw value, and
-// copies it, and then performs a toUpperCase() modification to the string.
-// Copying by the value, this means that it doesn't get in the way, or it doesn't modify or effect the original string.
-console.log(immutableString); // "I shall not change!"
-console.log(uppercaseString); // "I SHALL NOT CHANGE!"
+console.log(typeof 'Pepperoni'); // string
+console.log(typeof '0'); // string
+console.log('Pepperoni' instanceof String); // false
+console.log(new String('Pepperoni') instanceof String); // true -> with the key word 'new' we are creating an instance.
+console.log(String('Pepperoni') instanceof String); // false -> we are not creating an instance. We are using just static function String().
+console.log(Object.prototype.toString.call('Pepperoni')); // [object String] -> this is the best bulletproof way of detecting the typeof something!!!
 
 
