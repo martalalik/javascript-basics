897fb30fb52366f1f46eb2dd35e475f6b48a7f08
martalalik <lala.czaki@gmail.com>
2023-09-20 17:06:59 +0200

    Function Declarations and Expressions

--- src/index.js
+++ src/index.js
@@ -4,15 +4,44 @@ const app = document.getElementById('app');
 app.innerHTML = '<h1>JavaScript Basics</h1>';
 
 // ----------------------------------------------------------------
-// Booleans In-Depth
+// Functions In-Depth
 // ----------------------------------------------------------------
 
-// Correctly Type-Checking Booleans
+// Function Declarations and Expressions
 
-console.log(typeof true); // boolean
-console.log(typeof false); // boolean
-console.log(true instanceof  Boolean); // false
-console.log(new Boolean('X') instanceof  Boolean); // true
-// with booleans there is no need to go over the top with type safety, but this is what can be done to.
-console.log(Object.prototype.toString.call(true)); // [object Boolean]
+// Hoisting test
+console.log(makeCar); // only function declarations is seen
+// there are const, so we don't seed them before declaration.
+console.log(makeCarExpression2); // undefined - Variable might not have been initialized
+console.log(makeCarArrow); // undefined - Variable might not have been initialized
+console.log(makeCarArrowShorthand); // undefined - Variable might not have been initialized
 
+// 1: Function Declaration - mostly to create generic functions
+function makeCar() {
+  console.log('Making car...');
+}
+
+console.log(makeCar) // convert function to string, where we can see the structure of the function.
+makeCar(); // calling function
+
+// 2: Function Expression
+// const makeCarExpression1 = function makeCar() {}; // taking the value of a function.
+// it is not necessarily needed this makeCare keyword, so we get an anonymous function.
+const makeCarExpression2 = function () {}; // this is an anonymous function. Technically the function doesn't have a name.
+// function () {}; // you cannot create functions without a name. It needs a keyword to exist.
+console.log(makeCarExpression2); // makeCarExpression2() {}
+console.log(makeCarExpression2.name); // makeCarExpression2 - stringify name of the function, perhaps to pass somewhere.
+// named function expression
+const makeCarExpression3 = function myFunction() {};
+console.log(makeCarExpression3.name); // myFunction
+
+// 3: Arrow Function
+const makeCarArrow = () => {
+  console.log('Making car inside Arrow...');
+};
+
+makeCarArrow();
+
+// shorthand version
+const makeCarArrowShorthand = () => console.log('Short');
+makeCarArrowShorthand();
