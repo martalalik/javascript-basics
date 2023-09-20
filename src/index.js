917e9412a2422017b4f8632e4994008b847d4617
martalalik <lala.czaki@gmail.com>
2023-09-20 17:38:08 +0200

    Function Parameters and Defaults

--- src/index.js
+++ src/index.js
@@ -7,41 +7,25 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Functions In-Depth
 // ----------------------------------------------------------------
 
-// Function Declarations and Expressions
+// Function Parameters and Defaults
 
-// Hoisting test
-console.log(makeCar); // only function declarations is seen
-// there are const, so we don't seed them before declaration.
-console.log(makeCarExpression2); // undefined - Variable might not have been initialized
-console.log(makeCarArrow); // undefined - Variable might not have been initialized
-console.log(makeCarArrowShorthand); // undefined - Variable might not have been initialized
-
-// 1: Function Declaration - mostly to create generic functions
-function makeCar() {
-  console.log('Making car...');
+// declaring a function
+// name = parameter!!! -> is on the receiving end of the function!!!
+// 3: new feature in JS and preferable to assign default value to the parameter!!!
+function makeCar(name = 'Fiat') {
+  // adding conditional checks is a part of function design.
+  // 1:
+  // name = name || 'Fiat';
+  // 2:
+  // if(!name) {
+  //   name = 'Fiat';
+  // }
+  console.log(`Making car: ${name.toUpperCase()}`);
 }
 
-console.log(makeCar) // convert function to string, where we can see the structure of the function.
-makeCar(); // calling function
-
-// 2: Function Expression
-// const makeCarExpression1 = function makeCar() {}; // taking the value of a function.
-// it is not necessarily needed this makeCare keyword, so we get an anonymous function.
-const makeCarExpression2 = function () {}; // this is an anonymous function. Technically the function doesn't have a name.
-// function () {}; // you cannot create functions without a name. It needs a keyword to exist.
-console.log(makeCarExpression2); // makeCarExpression2() {}
-console.log(makeCarExpression2.name); // makeCarExpression2 - stringify name of the function, perhaps to pass somewhere.
-// named function expression
-const makeCarExpression3 = function myFunction() {};
-console.log(makeCarExpression3.name); // myFunction
-
-// 3: Arrow Function
-const makeCarArrow = () => {
-  console.log('Making car inside Arrow...');
-};
-
-makeCarArrow();
-
-// shorthand version
-const makeCarArrowShorthand = () => console.log('Short');
-makeCarArrowShorthand();
+// using the function
+// strings passed to the function are arguments!!!
+makeCar('Porsche');
+makeCar('Ferrari');
+makeCar(); // Uncaught TypeError: Cannot read properties of undefined (reading 'toUpperCase') -> the name parameter is undefined. To be safe it is good to create conditional checks.
+makeCar(''); // Fiat - with condition 1, 2 in place. '' -> when assign default value to the parameter.
