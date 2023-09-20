94716dfbac6afb9951fde70d0c103d11ec00c7c9
martalalik <lala.czaki@gmail.com>
2023-09-20 18:31:13 +0200

    Rest Parameters and Arguments

--- src/index.js
+++ src/index.js
@@ -7,25 +7,48 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Functions In-Depth
 // ----------------------------------------------------------------
 
-// Function Parameters and Defaults
+// Rest Parameters and Arguments
 
-// declaring a function
-// name = parameter!!! -> is on the receiving end of the function!!!
-// 3: new feature in JS and preferable to assign default value to the parameter!!!
-function makeCar(name = 'Fiat') {
-  // adding conditional checks is a part of function design.
-  // 1:
-  // name = name || 'Fiat';
-  // 2:
-  // if(!name) {
-  //   name = 'Fiat';
-  // }
-  console.log(`Making car: ${name.toUpperCase()}`);
+// Old way: using arguments is an old way of doing things.
+function makeCarPrice() {
+  // without specified parameters, passing arguments
+  console.log(arguments);
+  // to check if arguments is an array
+  console.log(Array.isArray(arguments)); // false
+  // but we can turn this into array, or iterate it.
+  console.log(arguments[0]); // 11
+  // 1: converting arguments into array - because it's array like  we can simply use Array.from() method, which returns a new array.
+  // 2: looping over arguments - .forEach
+  // Array.from(arguments).forEach(value => console.log(value)); // 11 44 55 99
+  // dynamically adding up values - .reduce() -> this method creates a new array based of all the items that we are passing through, so most likely we want return.
+  // prev, next - you can call it whatever
+  // Array.from(arguments).reduce((prev, next) => {
+  //   return prev + next;
+  // }); // because we are returning every single time here
+  // we can use const total
+  const total = Array.from(arguments).reduce((prev, next) => {
+    return prev + next;
+  });
+  // we can logout that one total
+  console.log(`Total: ${total}USD`); // Total: 231USD
 }
 
-// using the function
-// strings passed to the function are arguments!!!
-makeCar('Porsche');
-makeCar('Ferrari');
-makeCar(); // Uncaught TypeError: Cannot read properties of undefined (reading 'toUpperCase') -> the name parameter is undefined. To be safe it is good to create conditional checks.
-makeCar(''); // Fiat - with condition 1, 2 in place. '' -> when assign default value to the parameter.
+makeCarPrice(11, 44, 55, 99, 22); // it is possible to pass arguments to the function without specifying parameters on the function. Console.log wil print [[Prototype]]: Object ->
+// it's array like, because it doesn't have all of those properties that a typical array might. No access to Array methods
+// 22 - was added
+
+// ----------------------------------------------------------------
+
+// New way: new way of doing it, is to use REST PARAMETER!!! new way of accessing arguments. And it is an array, which makes things easier.
+// REST PARAMETER - gives rest of the arguments, always need to ba at the end.
+function makeCarPriceRest(numberOne, ...params) {
+  console.log(numberOne, params); // 99, [[ Prototype]]: Array[88, 77, 11, 44] -> gave us an Array
+  // checking if its is an Array
+  console.log(Array.isArray(params)); // true
+  // now i can use directly .reduce() method
+  const total = params.reduce((prev, next) => prev + next);
+  console.log(`Total: ${total}USD`); // Total: 220USD
+}
+
+makeCarPriceRest(99, 88, 77, 11, 44);
+
