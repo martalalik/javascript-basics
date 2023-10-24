4d560aa4d70ff349ea56f2f750574f2f2a89b41c
Marta Lalik <105340174+martalalik@users.noreply.github.com>
2023-10-24 11:11:32 +0200

    Correctly Type-Checking Objects

--- src/index.js
+++ src/index.js
@@ -7,25 +7,39 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Objects In-Depth
 // ----------------------------------------------------------------
 
-// Merging Objects
+// Correctly Type-Checking Objects
 
 const drink = {
     id: 'qwe123',
     name: 'Lemonade',
+    price: {
+        sale: 99,
+        full: 129
+    },
 };
 
-const price = {
-    sale: 99,
-    full: 129
-};
+// typeof
+console.log(typeof drink); // object
+console.log(typeof []); // object -> array is a special type of object
+console.log(typeof null); // object
 
-// 1. Object.assign({}, object, object, ...) -> in {} you can hardcode some values {id: 'abc'}
-// const mergedDrink = Object.assign({}, drink, price)
-// console.log(mergedDrink); // {id: 'qwe123', name: 'Lemonade', sale: 99, full: 129}
-// to get price parameter
-// const mergedDrink = Object.assign({}, drink, { price }); // {price} -> creating a new object with ES2015 syntax.
-// console.log(mergedDrink); // {id: 'qwe123', name: 'Lemonade', price: {sale: 99, full: 129}}
+// instanceof
+console.log({} instanceof Object); // true
+console.log([] instanceof Object); // true
+console.log(null instanceof Object); // false
 
-// 2. SPREAD OPERATOR ...object
-const mergedDrink = {...drink, ...{price}};
-console.log(mergedDrink); // {id: 'qwe123', name: 'Lemonade', price: {sale: 99, full: 129}}
+// Object.prototype.toString.call(object) -> strictly checking for an object, bulletproof type checking
+console.log(Object.prototype.toString.call(drink)); // [object Object]
+console.log(Object.prototype.toString.call([])); // [object Array]
+console.log(Object.prototype.toString.call(null)); // [object Null]
+
+// power of using function to type checking
+function getType(obj) {
+    return Object.prototype.toString
+        .call(obj)
+        .slice(8, -1)
+        .toLowerCase();
+}
+console.log(getType(drink)); // object
+console.log(getType([])); // array
+console.log(getType(null)); // null
