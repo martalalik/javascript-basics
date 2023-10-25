665096136e6a8634a2dde7d2e2027639c2118095
Marta Lalik <105340174+martalalik@users.noreply.github.com>
2023-10-25 11:46:43 +0200

    Declarative Object Iteration

--- src/index.js
+++ src/index.js
@@ -7,53 +7,10 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Objects In-Depth
 // ----------------------------------------------------------------
 
-// Imperative Object Iteration
+// Declarative Object Iteration
 
-// const drink = {
-//     id: 'qwe123',
-//     name: 'Lemonade',
-//     price: {
-//         sale: 99,
-//         full: 129
-//     },
-// };
-
-// - There is no guarantee that the loops will iterate through an object in order.
-// - Objects are for unordered data structure.
-
-// looking up into each property
-// for...in
-// for (const prop in drink) {
-    // console.log(prop); // id name price
-    // accessing the value of property
-    // console.log(drink[prop]); // id qwe123 name Lemonade price {sale: 99, full: 129}
-    // for (const key in drink[prop]) {
-    //     console.log(key); // 0 1 2 3 4 5 0 1 2 3 4 5 6 7 sale full -> all the indexes are printed, 0-5 for "qwe123" has 6 characters; 0-7 for "Lemonade" has 8 characters.
-    // }
-
-    // avoiding printing indexes
-    // 1. typeof
-    // if(typeof drink[prop] !== 'string') { // do not print anything that matches a string
-    //     for (const key in drink[prop]) {
-    //         console.log(key); // sale full
-    //     }
-    // }
-    // 2. Object.prototype.toString.call(object) -> much safer way!!!
-    // if(Object.prototype.toString.call(drink[prop]) === '[object Object]') {
-    //     for (const key in drink[prop]) {
-    //         console.log(key); // sale full
-    //     }
-    // }
-    // breaking into a function variable, to reference to only one property instead of looking at the object every single time
-    // const value = drink[prop];
-    // if(Object.prototype.toString.call(value) === '[object Object]') {
-    //     for (const key in value) {
-    //         console.log(key); // sale full
-    //     }
-    // }
-// }
-
-const drink2 = {
+const drink = {
+    id: 'qwe123',
     name: 'Lemonade',
     price: {
         sale: 99,
@@ -61,20 +18,18 @@ const drink2 = {
     },
 };
 
-// iterating through only properties that we want
-// Object.create(object) -> what happens here is: we are creating a new object from the prototype of existing object. drinkWithId inherited all drink properties.
-const drinkWithId = Object.create(drink2);
-// adding dynamically, id property to an inherited object.
-drinkWithId.id = 'qwe123';
-// console.log(drinkWithId); // {id: 'qwe123'} > [[Prototype]]: Object > name: "Lemonade" price {sale: 99, full: 129} -> here we can see inherited prototype.
-// checking for name in prototype
-// console.log('name' in drinkWithId); // true -> it does exist in drinkWithId prototype
-// IN operator - it looks up into prototype, it checks whether we have something on the prototype chain, so it will look up into [[Prototype]].
-for (const prop in drinkWithId ) {
-    // console.log(prop); // id name price -> so here we can see that, drinkWithId inherited properties from the drink2 object like name nad price. This is all because of IN operator.
-    // object.hasOwnProperty() -> it will iterate only through properties that exist on the drinkWithId object and won't iterate through [[Prototype]].
-    if(drinkWithId.hasOwnProperty(prop)) {
-        console.log(prop); // id -> it has only id, because we iterate properties directly on the object, and we are ignoring properties exists in the prototype.
-        console.log(drinkWithId[prop]); // qwe123
-    }
-}
+// Object.keys(object)
+// console.log(Object.keys(drink)); // (3) -> array of all top level keys, but it does shallow iteration, it won't go deep into price. It doesn't look at the [[Prototype]], so it won't return any inherited properties.
+// because Object.keys(drink) returns array we can use forEach. This is a declarative way that we can iterate over a drink object.
+// Object.keys(drink).forEach(function(prop, index) {
+//     console.log(prop); // id name price
+//     console.log(index); // 0 1 2
+// });
+Object.keys(drink).forEach(function(prop) {
+    console.log(drink[prop]); // qwe123 Lemonade {sale: 99, full: 129} -> getting values, no inherit values
+});
+// arrow function
+Object.keys(drink).forEach(prop => console.log(drink[prop]));
+
+// Object.entries(object)
+console.log(Object.entries(drink)); // 0:(2) ['id', 'qwe123'] 1:(2) ['name', 'Lemonade'] 2:(2) ['price', {…}] -> returns array of arrays
