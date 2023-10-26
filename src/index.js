c406278765f8a3fef6d79666552fae7d8d5e34f2
Marta Lalik <105340174+martalalik@users.noreply.github.com>
2023-10-26 09:14:27 +0200

    Multi-dimensional Arrays

--- src/index.js
+++ src/index.js
@@ -7,23 +7,34 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Arrays In-Depth
 // ----------------------------------------------------------------
 
-// Multi-dimensional Arrays
-
-// - Multi-dimensional Array is array in an array [[]]
+// Properties, Indexes and Elements
 
 // ELEMENTS - things that we put in an array
-const drinks = [['Lemonade', 99], ['Lime', 99], ['Peach', 79]];
-console.log(drinks); // (3)[Array(2), Array(2), Array(2)]
+const drinks = ['Lemonade', 'Lime', 'Peach'];
 
-// ACCESSING inner array
-const drinkOne = drinks[0];
-const drinkTwo = drinks[1];
-const drinkThree = drinks[2];
-console.log(drinkOne); // (2)['Lemonade', 99]
-console.log(drinkTwo); // (2)['Lime', 99]
-console.log(drinkThree); // (2)['Peach', 79]
-// login out the value
-console.log(`Drink: ${drinkOne[0]}, Price: ${drinkOne[1]}`); // Drink: Lemonade, Price: 99
-// getting value direct from drinks
-console.log(`Drink: ${drinks[0][0]}, Price: ${drinks[0][1]}`); // Drink: Lemonade, Price: 99
+// INDEXES
+// accessing arrays elements by using indexes lookups
+console.log(drinks); // 0:"Lemonade" 1:"Lime" 2:"Peach" -> array elements are indexed in order in the same way that strings also are.
+console.log(drinks[0]); // Lemonade -> use number format
+console.log(drinks['0']); // Lemonade
 
+// accessing LAST ELEMENT in the array
+console.log(drinks); // 0:"Lemonade" 1:"Lime" 2:"Peach" length:3
+console.log(drinks[drinks.length - 1]); // 2:"Peach" -> this approach is dynamic
+
+// accessing an item and replace the value
+drinks[0] = 'Diet Lemonade';
+console.log(drinks); // (3)['Diet Lemonade', 'Lime', 'Peach']
+
+// we can use string to set values in the array using words instead of numbers
+// drinks['favourite'] = 'Cola'; // here we also added a property 'favourite'. Avoid using this because it looks like an object, and maybe it should be and when we reset an array drinks.length = 0, the favourite:"Cola" will still exist on the array.
+// console.log(drinks); // ['Diet Lemonade', 'Lime', 'Peach', favourite: 'Cola'] -> arrays are a special type of object, has properties which are 0, 1, 2, favourite
+
+// RESET & EMPTY an array
+// 1. length - preferred
+// drinks.length = 0; // resetting array
+// console.log(drinks); // favourite:"Cola" (when favourite:"Cola") / []
+// 2. splice() -> removes elements
+drinks.splice(0, drinks.length); // 0 -> it starts at the front of the array. drinks.length -> as many items as that are in the length
+console.log(drinks); // []
+//
