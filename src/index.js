1dd40c2d0206347043c38747abfbed68f6252fdd
Marta Lalik <105340174+martalalik@users.noreply.github.com>
2023-10-26 13:19:30 +0200

    Removing Array Elements

--- src/index.js
+++ src/index.js
@@ -7,41 +7,47 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Arrays In-Depth
 // ----------------------------------------------------------------
 
-// Adding Array Elements
+// Removing Array Elements
 
 const drinks = ['Lemonade', 'Lime', 'Peach'];
 
-// MUTATING EXISTING ARRAY - it is not a good approach because arrays are passed by reference. It mutates original array drinks.
-
-// BEGINNING unshift() -> adds elements at the beginning of an array
-// drinks.unshift('Water');
-// console.log(drinks); // (4)['Water', 'Lemonade', 'Lime']
-
-// MIDDLE splice() -> removes elements from an array and, if necessary, inserts new elements in their place.
-const index = 1; //
-// drinks.splice(index, 0, 'Cola');
-// console.log(drinks); // (4)['Lemonade', 'Cola', 'Lime', 'Peach']
-
-// END push() -> adds the specified elements to the end of an array
-// drinks.push('Cola');
-// console.log(drinks); // (5)['Water', 'Lemonade', 'Lime', 'Cola']
-
-// IMMUTABLE -> expecting a new array back. We copy the original array and mutate copy of that array
-// BEGINNING & END
-const newDrinks = ['Water', ...drinks, 'Cola']; // copy drinks to new drinks and adds a new element
-// console.log(newDrinks); // (4)['Water', 'Lemonade', 'Lime', 'Peach', 'Cola']
-
-// RESTING an array
-newDrinks.length = 0;
-// console.log(newDrinks); // []
+// MUTABLE - it is not a good approach because arrays are passed by reference. It mutates original array drinks.
+// BEGINNING - shift() -> method removes the first element from an array and returns that removed element. This method changes the length of the array.
+// 1.
+// const removed = drinks.shift();
+// console.log(removed); // Lemonade
+// 2.
+// drinks.shift();
+// console.log(drinks); // Lemonade
 
 // MIDDLE
-// we are cutting the array in half
-console.log([
-    ...drinks.splice(0, index), // ['Lemonade'] -> 0: capture the beginning of the array; index: deleting everything after a first element
-    'Mojito', // adding an element in the middle
-    ...drinks.splice(index -1) // (3)['Lemonade', 'Lime', 'Peach']
-]); // (4)['Lemonade', 'Mojito', 'Lime', 'Peach']
+
+// END pop() -> method removes the last element from an array and returns that element. This method changes the length of the array.
+const removedEnd = drinks.pop();
+console.log(removedEnd); // Peach
+
+// IMMUTABLE -> expecting a new array back. We copy the original array and mutate copy of that array
+// slice() -> remove an item from anywhere in array
+// 1. without defined index
+// const newDrinks = [
+//     ...drinks.slice(0, 0),
+//     ...drinks.slice(1)
+// ];
+// console.log(newDrinks); // (2)['Lime', 'Peach']
+// 2. with defined index
+// const index = 0;
+// const newDrinks = [
+//     ...drinks.slice(0, index),
+//     ...drinks.slice(index + 1)
+// ];
+// console.log(newDrinks); // (2)['Lime', 'Peach']
+// 3. end
+// const index = drinks.length -1;
+// const newDrinks = [
+//     ...drinks.slice(0, index),
+//     ...drinks.slice(index + 1)
+// ];
+// console.log(newDrinks); // (2)['Lemonade', 'Lime']
 
 console.log(drinks); // (3)['Lemonade', 'Lime', 'Peach'] -> when immutable, it shows that array is not touched
 
