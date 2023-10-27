e41cdb752f85c044a29d3f10b7b690372c1839ab
Marta Lalik <105340174+martalalik@users.noreply.github.com>
2023-10-27 13:12:44 +0200

    Iteration with Break and Continue

--- src/index.js
+++ src/index.js
@@ -7,14 +7,20 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Arrays In-Depth
 // ----------------------------------------------------------------
 
-// Imperative Array Iteration
+// Iteration with Break and Continue
 
 const drinks = ['Pepsi', 'Lemonade', 'Cola'];
+let count = 0;
 
 // for(new_variable; instruction how long we want to continue the loop; increment 0){}
 for(let i = 0; i < drinks.length; i++) {
     const drink = drinks[i];
     if(drink === 'Lemonade') {
         console.log(`Pour me a glass`);
+        // break;
+        continue; // does things opposite to a break statement
     }
+    count++; // incrementing count at any point in time
 }
+
+console.log(count); // 3 -> without_break; 1 -> with_break; 2 -> with_continue
