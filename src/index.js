8fdce2791d890c6400ccd2cdf8cc36eb524d9e6b
Marta Lalik <105340174+martalalik@users.noreply.github.com>
2023-10-27 11:14:52 +0200

    Merging Arrays

--- src/index.js
+++ src/index.js
@@ -7,37 +7,27 @@ app.innerHTML = '<h1>JavaScript Basics</h1>';
 // Arrays In-Depth
 // ----------------------------------------------------------------
 
-// Shallow and Deep Array Cloning
+// Merging Arrays
 
-const drinks = [['Lemonade', 99], ['Lime', 89], ['Peach', 79]];
+const drinks = [['Lemonade', 99], ['Lime', 89]];
+const newDrinks = [['Peach', 79]];
+const simpleDrinks = ['Peach', 79];
 
-// SHALLOW -> will go 1 level down, which means that it won't clone nested arrays, only the top level. That is why it will mutate nested elements in an array, even if we mutate values on a cloned array.
-// 1. cloning with spread operator: ...drinks
-// const drinksClone = [...drinks]; // this is a direct copy of the drinks array.
-// we are inside our top level drinks.
-// console.log(drinks[0]); // (2)['Lemonade', 99] -> accessing the first item of an array inside the array.
-// console.log(drinks[0][0]); // Lemonade -> accessing the string.
-// console.log(drinks[0][1]); // 99 -> accessing the number.
+// OLD WAY
+// concat(): (string | number)[][] -> method is used to merge two or more arrays. This method does not change the existing arrays, but instead returns a new array. Immutable way to merge arrays.
+// const merge = drinks.concat(newDrinks);
+// console.log(merge); // (3)[Array(2), Array(2), Array(2)]
+// console.log(drinks); // (2)[Array(2), Array(2)]
+// console.log(newDrinks); // [Array(2)]
 
-// looking how change effects arrays:
-// drinksClone[0][1] = 100;
-// console.log(drinksClone); // 0:(2) ['Lemonade', 100] ...
-// console.log(drinks); // 0:(2) ['Lemonade', 100] ... -> the original array gets mutated as well, this is not good behavior that needs to be avoided.
-
-// 2. cloning with slice()
-// const drinksClone2 = drinks.slice(); // without passing any arguments, we clone the whole array.
-// drinksClone2[0][1] = 100;
-// console.log(drinksClone2); // 0:(2) ['Lemonade', 100] ...
-// console.log(drinks); // 0:(2) ['Lemonade', 100] ... -> the original array gets mutated as well, this is not good behavior that needs to be avoided.
-
-// 3. cloning with Array.from(object):
-// const drinksClone3 = Array.from(drinks);
-// drinksClone3[0][1] = 100;
-// console.log(drinksClone3); // 0:(2) ['Lemonade', 100] ...
-// console.log(drinks); // 0:(2) ['Lemonade', 100] ... -> the original array gets mutated as well, this is not good behavior that needs to be avoided.
-
-// DEEP
-const drinksClone = JSON.parse(JSON.stringify(drinks)); // need to parse it, because JSON.stringify() returns a string
-drinksClone[0][1] = 100;
-console.log(drinksClone); // 0:(2) ['Lemonade', 100] ...
-console.log(drinks); // 0:(2) ['Lemonade', 99] ...
+// NEW WAY
+// ... : spread operator
+const merge2 = [...drinks, ...newDrinks];
+console.log(merge2); // (3)[Array(2), Array(2), Array(2)]
+console.log(drinks); // (2)[Array(2), Array(2)]
+console.log(newDrinks); // [Array(2)]
+// merging multidimensional array with a one-dimensional array -> we want merge the value of one-dimensional array not the initial array
+const merge3 = [...drinks, simpleDrinks]; // by removing ...
+const merge4 = [simpleDrinks, ...drinks]; // you can swap order
+console.log(merge3); // (3)[Array(2), Array(2), Array(2)], Peach at the end
+console.log(merge4); // (3)[Array(2), Array(2), Array(2)], Peach at the beginning
