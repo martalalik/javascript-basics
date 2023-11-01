cb5f05676f0d06d419a0f9cbeb3ee786ee87c926
Marta Lalik <105340174+martalalik@users.noreply.github.com>
2023-11-01 11:25:26 +0100

    Exploring Array.filter

--- src/index.js
+++ src/index.js
@@ -1,53 +1,41 @@
 import '../assets/css/style.css';
 
 const app = document.getElementById('app');
-app.innerHTML = '<h1>Array.prototype.map()</h1>';
+app.innerHTML = '<h1>Array.prototype.filter()</h1>';
 
 // ----------------------------------------------------------------
 // Arrays In-Depth
 // ----------------------------------------------------------------
 
-// Exploring Array.map
-// for BIG PROBLEMATIC, DATA STRUCTURE!!!
+// Exploring Array.filter
 
-// DECLARATIVE style -> telling what to do.
-// - map() -> is a method that exists on array prototype.
-// - Allows to loop over an array and access each individual item much like a forEach. However, we can return newVALUES or newTYPES of the values for each item in array.
-// - Avery item that is returned it will give NEW ARRAY -> containing new copies or changes of each array element. You can return specific property as a new array.
-// - map() is a common solution to deal with lots of data structure problems. it allows a value of any type from primitive to objects, giving some great flexibility in the programming.
-// - Syntax: const returnedValue = items.map((value, index, array) => {...need to RETURN something}, thisArg);
-// - Using array.map() is called an IMMUTABLE PATTERN. As it creates a new array from an existing array. halfOffFries has nothing to do with items array.
-
+// - Array.filter() -> it's a method that exists on Array.prototype.filter().
+// - Allows CONDITIONALLY RETURN certain elements from an array into a brand-new array.
+// - It's a commonly use to REMOVES ITEMS from an array, by simply excluding them from the result; I want a new array just containing the items that I need.
+// - RETURN an expression that filter() evaluates to either false or true.
+// - Callbacks returns an expression that evaluates to true of false.
+// - TRUE -> array elements that evaluate to true are stored in the new array.
+// - FALSE -> array elements that evaluate to false are excluded from the new array.
+// - Once the filter() has completed, you can access the new array of values.
+// - Syntax: const returnedValues = array.filter((value, index, array) => {...}, thisArg);
+// - You can think about filter() sa a FUNCTIONAL IF STATEMENT -> if my array element, meets a particular criteria, we can ask for a copy of it back.
 const items = [
     { id: '🍔', name: 'Super Burger', price: 399 },
     { id: '🍟', name: 'Jumbo Fries', price: 199 },
     { id: '🥤', name: 'Big Slurp', price: 299 }
 ];
 
-const halfOffFries = items.map((item) => {
-    if(item.id === '🍟'){
-        return {
-            ...item, // with ... operator we are getting the whole object
-            price: item.price / 2
-        };
-    }
-    // to fix undefined for a first and last element of an array, return item;
-    return item;
-});
+const expensiveItems = items.filter((item) => item.price > 199);
+console.log(expensiveItems); // (2)[{…}, {…}] -> new array
+console.log(items); // (3)[{…}, {…}, {…}] -> untouched
 
-console.log(halfOffFries); // 0: undefined 1:{..., price: 99,5} 2:undefined -> without return item; // 0: {} 1:{..., price: 99,5} 2:{} -> with return item;
-
-// IMPERATIVE style -> explaining how to do it.
-// const halfOffFries = [];
+// old way of doing the same as filter()
+// const expensiveItems = [];
 // for(let i = 0; i < items.length; i++) {
 //     const item = items[i];
-//     if(item.id === '🍟'){
-//         halfOffFries.push({
-//             ...item,
-//             price: item.price / 2,
-//         });
-//     } else {
-//         halfOffFries.push(item);
+//     if(item.price > 199){
+//         expensiveItems.push(item);
 //     }
 // }
-// console.log(halfOffFries);
+// console.log(expensiveItems); // (2)[{…}, {…}] -> new array
+// console.log(items); // (3)[{…}, {…}, {…}] -> untouched
