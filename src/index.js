2c7a25b7492a3b16bed91dd1dab010df91dbc077
Marta Lalik <105340174+martalalik@users.noreply.github.com>
2023-11-01 15:14:01 +0100

    Exploring Array.every

--- src/index.js
+++ src/index.js
@@ -1,47 +1,43 @@
 import '../assets/css/style.css';
 
 const app = document.getElementById('app');
-app.innerHTML = '<h1>Array.prototype.some()</h1>';
+app.innerHTML = '<h1>Array.prototype.every()</h1>';
 
 // ----------------------------------------------------------------
 // Arrays In-Depth
 // ----------------------------------------------------------------
 
-// Exploring Array.some
+// Exploring Array.every
 
-// - Exists on Array.prototype.some().
-// - Tells whatever any element in our array passes a particular test, then will give true, otherwise false.
-// - Think: I want to check if any of my values in my array meet a specific condition. Either one of some values needs to pass to get tru in return.
-// - Doesn't modify the array.
-// - It returns only boolean.
-// - syntax: const returnedValues = array.some((value, index, array) => {...}, thisArg);
-// - It will loop through the array and do the checking on each value of an array.
+// - Exists on Array.prototype.every().
+// - Tells whether every single element inside of an array passes a particular test, and that test it up to you.
+// - TURE -> If EVERY element passes, once it's finished the loop.
+// - FALSE -> If ONE element will fail.
+// - The looping will stop after a first failing element. This is very good for performance.
+// - syntax: const returnedValue = array.every((value, index, array) => {...}, thisArg);
 
 const items = [
-    { id: '🍔', name: 'Super Burger', price: 399, promo: false },
-    { id: '🍟', name: 'Jumbo Fries', price: 199, promo: false },
-    { id: '🥤', name: 'Big Slurp', price: 299, promo: true }
+    { id: '🍔', name: 'Super Burger', price: 399, stock: true },
+    { id: '🍟', name: 'Jumbo Fries', price: 199, stock: true },
+    { id: '🥤', name: 'Big Slurp', price: 299, stock: false }
 ];
 
-const isInPromo = items.some((item) => item.promo);
-console.log(isInPromo); // true
-
-// getting discount on products, when buy all and drinks in under promo
-// 1. find if there is anything in promo -> which we did with isInPromo
-// 2. getting a correct price.
-const total = isInPromo ? 600 : items.reduce((prev, next) => prev + next.price, 0);
-console.log(total); // 600
+const isInStock = items.every((item) => item.stock);
+if(!isInStock) {
+    console.log(`Sorry, ${items.find((item) => ! item.stock).name} is out of stock`); // Sorry, Big Slurp is out of stock
+}
 
 // IMPERATIVE style
-// let isInPromo = false;
-// for(let i = 0; i< items.length; i++) {
+// let isInStock = true;
+// for (let i = 0; i < items.length; i++) {
 //     const item = items[i];
-//     if(item.promo) {
-//         isInPromo = true;
+//     if(!item.stock) {
+//         isInStock = false;
 //         break;
 //     }
 // }
-// const total = isInPromo ? 600 : items.reduce((prev, next) => prev + next.price, 0);
-// console.log(total); // 600
+// if(!isInStock) {
+//     console.log(`Sorry, ${items.find((item) => ! item.stock).name} is out of stock`); // Sorry, Big Slurp is out of stock
+// }
 
 console.log(items);
